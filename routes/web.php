89251280ad9ada284a5bb94c547d4786f5948c66
MdIshtiaque <ishtiaqueferdous109@gmail.com>
2024-05-10 17:42:46 +0600

    Full Task Done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('users', [AuthController::class, 'createUser'])->name('create.user');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+
+/**
+ * Show all the deposited transactions.
+ */
+Route::get('show-all-deposited-transactions',[TransactionController::class, 'showAllDepositedTransactions'])->name('show.all.deposited.transactions');
+
+
+/**
+ * deposit a new amount
+ */
+Route::post('deposit', [TransactionController::class, 'deposit'])->name('deposit');
+
+/**
+ * show all the withdrawal transactions.
+ */
+Route::get('show-all-withdrawal-transactions', [TransactionController::class, 'showAllWithdrawalTransactions'])->name('show.all.withdrawal.transactions');
+
+
+/**
+ * withdraw a new amount
+ */
+Route::post('withdrawal', [TransactionController::class, 'withdrawal'])->name('withdrawal');
